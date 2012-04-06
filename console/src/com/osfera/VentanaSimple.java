7d6f2cfbb430bd5efe914d45725350156a58cbc5
Luis Palomero <lpalomero@gmail.com>
2012-04-06 19:07:29 +0200

    v0.22 - Algunas mejoras al layout

--- console/src/com/osfera/VentanaSimple.java
+++ console/src/com/osfera/VentanaSimple.java
@@ -15,6 +15,12 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.FlowLayout;
+import javax.swing.JFormattedTextField;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.BoxLayout;
 
 public class VentanaSimple {
 
@@ -49,16 +55,29 @@ public class VentanaSimple {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 500, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{165, 341, 0};
-		gridBagLayout.rowHeights = new int[]{21, 101, 35, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{250, 250, 0, 0};
+		gridBagLayout.rowHeights = new int[]{10	, 100, 100, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
+		JTextPane textPane = new JTextPane();
+		GridBagConstraints gbc_textPane = new GridBagConstraints();
+		gbc_textPane.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane.gridx = 0;
+		gbc_textPane.gridy = 0;
+		frame.getContentPane().add(textPane, gbc_textPane);
+		
 		JButton btnEncender = new JButton("Encendido");
+		GridBagConstraints gbc_btnEncender = new GridBagConstraints();
+		gbc_btnEncender.fill = GridBagConstraints.BOTH;
+		gbc_btnEncender.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEncender.gridx = 0;
+		gbc_btnEncender.gridy = 1;
+		frame.getContentPane().add(btnEncender, gbc_btnEncender);
 		btnEncender.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -66,23 +85,13 @@ public class VentanaSimple {
 			}
 		});
 		
-		JTextPane textPane = new JTextPane();
-		GridBagConstraints gbc_textPane = new GridBagConstraints();
-		gbc_textPane.anchor = GridBagConstraints.NORTH;
-		gbc_textPane.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textPane.insets = new Insets(0, 0, 5, 0);
-		gbc_textPane.gridwidth = 2;
-		gbc_textPane.gridx = 0;
-		gbc_textPane.gridy = 0;
-		frame.getContentPane().add(textPane, gbc_textPane);
-		GridBagConstraints gbc_btnEncender = new GridBagConstraints();
-		gbc_btnEncender.fill = GridBagConstraints.BOTH;
-		gbc_btnEncender.insets = new Insets(0, 0, 5, 5);
-		gbc_btnEncender.gridx = 0;
-		gbc_btnEncender.gridy = 1;
-		frame.getContentPane().add(btnEncender, gbc_btnEncender);
-		
 		JButton btnApagar = new JButton("Apagado");
+		GridBagConstraints gbc_btnApagar = new GridBagConstraints();
+		gbc_btnApagar.fill = GridBagConstraints.BOTH;
+		gbc_btnApagar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnApagar.gridx = 1;
+		gbc_btnApagar.gridy = 1;
+		frame.getContentPane().add(btnApagar, gbc_btnApagar);
 		btnApagar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -90,40 +99,41 @@ public class VentanaSimple {
 				cliente.getMainInstance().apaga();
 			}
 		});
-		GridBagConstraints gbc_btnApagar = new GridBagConstraints();
-		gbc_btnApagar.fill = GridBagConstraints.BOTH;
-		gbc_btnApagar.insets = new Insets(0, 0, 5, 0);
-		gbc_btnApagar.gridx = 1;
-		gbc_btnApagar.gridy = 1;
-		frame.getContentPane().add(btnApagar, gbc_btnApagar);
-		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel.anchor = GridBagConstraints.NORTH;
-		gbc_panel.gridwidth = 2;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 2;
-		frame.getContentPane().add(panel, gbc_panel);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JButton btnScaleUp = new JButton("Aumenta");
-		panel.add(btnScaleUp);
-		
-		JButton btnScaleDown = new JButton("Reduce");
-		btnScaleDown.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				cliente.getMainInstance().cambiaSmall();
-			}
-		});
-		panel.add(btnScaleDown);
+		GridBagConstraints gbc_btnScaleUp = new GridBagConstraints();
+		gbc_btnScaleUp.anchor = GridBagConstraints.EAST;
+		gbc_btnScaleUp.insets = new Insets(0, 0, 5, 5);
+		gbc_btnScaleUp.gridx = 0;
+		gbc_btnScaleUp.gridy = 2;
+		frame.getContentPane().add(btnScaleUp, gbc_btnScaleUp);
 		btnScaleUp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				cliente.getMainInstance().cambiaLarge();
 			}
 		});
+		
+		JButton btnScaleDown = new JButton("Reduce");
+		GridBagConstraints gbc_btnScaleDown = new GridBagConstraints();
+		gbc_btnScaleDown.anchor = GridBagConstraints.WEST;
+		gbc_btnScaleDown.insets = new Insets(0, 0, 5, 5);
+		gbc_btnScaleDown.gridx = 1;
+		gbc_btnScaleDown.gridy = 2;
+		frame.getContentPane().add(btnScaleDown, gbc_btnScaleDown);
+		btnScaleDown.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				cliente.getMainInstance().cambiaSmall();
+			}
+		});
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		GridBagConstraints gbc_formattedTextField = new GridBagConstraints();
+		gbc_formattedTextField.insets = new Insets(0, 0, 5, 0);
+		gbc_formattedTextField.gridx = 2;
+		gbc_formattedTextField.gridy = 2;
+		frame.getContentPane().add(formattedTextField, gbc_formattedTextField);
 	}
 
 }
